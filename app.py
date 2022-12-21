62f33886523296124fe7e209db43544677027fdf
Jacob Seol <jacobseol96@gmail.com>
2022-12-20 22:02:42 -0800

    refator GUI layout hierarchy to account for moving Labels

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QFrame, QGridLayout, QHBoxLayout
-from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QFrame, QGridLayout, QStackedLayout
+from PyQt5.QtCore import Qt
 from board import Board
+from custom_label import ChessPieceLabel
 
 class ChessGUI(QMainWindow):
     def __init__(self, chess_board):
         super().__init__()
         self.chess_board = chess_board # internal chess board
 
+        # create and set dimensions of the GUI
         self.setWindowTitle("Chess Engine")
         self.setGeometry(0, 0, 800, 800) # (x, y, width, height)
-
+        self.setFixedSize(800, 800)
+        
+        # where the main content goes
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
 
@@ -19,7 +23,7 @@ class ChessGUI(QMainWindow):
         self.grid_layout.setContentsMargins(0, 0, 0, 0) # controls the spacing around the contents of the layout
         self.central_widget.setLayout(self.grid_layout)
 
-        self.board = [[QFrame(self.central_widget) for _ in range(8)] for _ in range(8)] # specify the parent widget when instantiating a QFrame
+        self.board = [[QFrame(self.central_widget) for _ in range(8)] for _ in range(8)] # creates a QFrame with the central_widget as its parent
         for i in range(8):
             for j in range(8):
                 if (i + j) % 2 == 0:
@@ -37,12 +41,17 @@ class ChessGUI(QMainWindow):
             for j in range(8):
                 piece = self.chess_board.grid[i][j]
                 if piece:
-                    label = QLabel()
+                    label = ChessPieceLabel() # creates a QLabel with a QFrame as its parent (we can do this conversely as well)
                     label.setPixmap(piece.get_pixmap())
                     label.setScaledContents(True)
-                    self.board[i][j].setLayout(QHBoxLayout()) 
-                    self.board[i][j].layout().addWidget(label)
-
+                    label.setFixedSize(100, 100)
+                    label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+                    self.grid_layout.addWidget(label, i, j)
+                # else:
+                #     square = QFrame()
+                #     square.setFrameStyle(QFrame.Panel | QFrame.Plain)
+                #     self.grid_layout.addWidget(square, i, j)
+                    
     def move_piece(self, from_pos, to_pos):
         self.chess_board.move_piece(from_pos, to_pos)
         self.update_board()
